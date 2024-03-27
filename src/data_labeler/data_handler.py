22931132388b3c6c17ef87f068ec42cda1c106cf
kalouskova <veronika.kalouskova@hamil.com>
2024-03-28 00:43:23 +0100

    default artifact value based on activity type, data form first three people

--- src/data_labeler/data_handler.py
+++ src/data_labeler/data_handler.py
@@ -14,7 +14,7 @@ class DataHandler():
 
     def __init__(self, filename, fs, seg_len):
         self.FILE_IN = filename
-        self.FILE_OUT = '../data/labels/' + filename.split('.')[0] + '_' + str(fs) + '_' + str(seg_len) + '_labels.csv'
+        self.FILE_OUT = '../data/labels/' + filename.split('.')[0] + '_labels_' + str(seg_len) + '.csv'
 
         self.df_in = self.read_file(filename)
         self.load_data(fs, seg_len)
@@ -24,6 +24,10 @@ class DataHandler():
         if os.path.exists(self.FILE_OUT):
             self.df_out = pd.read_csv(self.FILE_OUT, sep=';')
         else:
+            self.create_df(fs, seg_len)
+
+    #   Create output dataframe
+    def create_df(self, fs, seg_len):
             data_size = len(self.df_in['value'])
 
             seg_len_pts = seg_len * fs
@@ -40,9 +44,15 @@ class DataHandler():
             self.df_out['end'] = end
             self.df_out['activity'] = type
 
+            # Default values based on activity type - rest defaults to no artifact, other activities to artifact present
+            if (type == 0):
+                self.df_out['artifact'] = 0
+            else:
+                self.df_out['artifact'] = 1
+
     #   Set value of artifact based on radio button selection
     def set_artifact(self, seg_curr, label):
-        self.df_out.at[seg_curr, 'artifact']  = label
+        self.df_out.at[seg_curr, 'artifact']  = int(label)
         self.df_out.to_csv(self.FILE_OUT, sep=';', index=False)  
 
         print(self.df_out)
@@ -56,16 +66,18 @@ class DataHandler():
    
     #   Determine activity type based on filename
     def get_activity_type(self):
-        if 'rest' in self.FILE_IN:
+        if 'klud' in self.FILE_IN:
             return 0, 'REST'
-        elif 'walk' in self.FILE_IN:
-            return 1, 'WALK'
-        elif 'run5' in self.FILE_IN:
-            return 2, 'RUN 5 km/h'
-        elif 'squats' in self.FILE_IN:
-            return 3, 'SQUATS'
+        elif 'ruky' in self.FILE_IN:
+            return 1, 'ARM MOVEMENTS'
+        elif 'chodza' in self.FILE_IN:
+            return 2, 'WALK 4 km/h'
+        elif 'beh' in self.FILE_IN:
+            return 3, 'RUN 8 km/h'
+        elif 'drepy' in self.FILE_IN:
+            return 4, 'SQUATS'
         else:
-            return 4, 'UNKNOWN'
+            return 5, 'UNKNOWN'
 
     #   Read input .csv file, handle possible exceptions
     def read_file(self, filename):
